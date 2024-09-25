532e17e9b1ca81d5cde646cd3b0f56e6e185097a
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-09-25 08:21:59 +0000

    use tracing-appender

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,30 @@
 use clap::Parser;
 use serde::Deserialize;
 
+use crate::LOKAI_DIR;
+
+static OLLAMA_URL: &str = "http://localhost:11434";
+static DEFAULT_LLM_MODEL: &str = "phi3:3.8b";
+
 #[derive(Deserialize, Debug)]
 pub struct AppConfig {
+    // TODO: replace with Kalosm
     ollama_url: String,
+    // TODO: I need to be opinionated and allow to define only one model
     default_llm_model: String,
+    // TODO: rename to sqlite_url
     database_url: String,
 }
 
-impl Default for AppConfig {
-    fn default() -> Self {
+impl AppConfig {
+    pub fn init() -> Self {
         Self {
-            ollama_url: "http://localhost:11434".into(),
-            default_llm_model: "phi3:3.8b".into(),
-            database_url: "sqlite::memory:".into(),
+            ollama_url: "".to_string(),
+            default_llm_model: "".to_string(),
+            database_url: "".to_string(),
         }
     }
-}
 
-impl AppConfig {
     pub fn get_ollama_url(&self) -> &str {
         &self.ollama_url
     }
@@ -29,10 +35,6 @@ impl AppConfig {
 
     pub fn get_database_url(&self) -> &str {
         &self.database_url
-    }
-
-    pub fn set_default_llm_model(&mut self, default_llm_model: String) {
-        self.default_llm_model = default_llm_model;
     }
 
     pub fn update_from_cli_args(&mut self, cli_args: AppConfigCliArgs) {
@@ -58,7 +60,24 @@ pub struct AppConfigCliArgs {
     /// Default LLM Model user want to use
     #[arg(long)]
     default_llm_model: Option<String>,
-    /// Sqlite database URL
+    /// Sqlite database URL ["sqlite::memory:" (in-memory), "sqlite://db.slite3" (persistent), "db.sqlite3" (persitent)]
     #[arg(long)]
     database_url: Option<String>,
+}
+
+impl From<AppConfigCliArgs> for AppConfig {
+    fn from(value: AppConfigCliArgs) -> Self {
+        Self {
+            ollama_url: value.ollama_url.unwrap_or_else(|| OLLAMA_URL.to_string()),
+            default_llm_model: value
+                .default_llm_model
+                .unwrap_or_else(|| DEFAULT_LLM_MODEL.to_string()),
+            database_url: value.database_url.unwrap_or_else(|| {
+                format!(
+                    "sqlite://{}/db.sqlite",
+                    LOKAI_DIR.to_str().expect("Cannot get lokai config dir")
+                )
+            }),
+        }
+    }
 }
